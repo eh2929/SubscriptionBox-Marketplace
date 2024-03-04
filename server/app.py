123fa36464d9d27eac3c94ec2a3b49d6c5bbadab
Andrew Wilson <adwilson12901@gmail.com>
2024-03-04 16:15:40 -0500

    added models import and serializer mixin

--- server/app.py
+++ server/app.py
@@ -3,19 +3,20 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
-from flask_restful import Resource
+from flask import request, make_response
+from flask_restful import Api, Resource
 
 # Local imports
 from config import app, db, api
 # Add your model imports
+from models import User, Order, Subscription, Box
 
 
 # Views go here!
 
 @app.route('/')
 def index():
-    return '<h1>Project Server</h1>'
+    return '<h1>Project Server</h1><p>Change the endpoint to see data.</p>'
 
 
 if __name__ == '__main__':
