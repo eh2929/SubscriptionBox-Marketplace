388c19f793f909c7bfd8f968ff11884e6e695891
Andrew Wilson <adwilson12901@gmail.com>
2024-03-05 11:53:32 -0500

    routes and comments

--- server/app.py
+++ server/app.py
@@ -5,18 +5,146 @@
 # Remote library imports
 from flask import request, make_response
 from flask_restful import Api, Resource
-
 # Local imports
 from config import app, db, api
-# Add your model imports
 from models import User, Order, Subscription, Box
-
+# Initialize Api
+api = Api(app)
 
 # Views go here!
-
+# Routes
 @app.route('/')
 def index():
     return '<h1>Project Server</h1><p>Change the endpoint to see data.</p>'
+
+# '/subscriptions' route
+class Subscriptions(Resource):
+    def get(self):
+        subs = [sub.to_dict() for sub in Subscription.query.all()]
+        response = make_response(
+            subs,
+            200
+        )
+        return response
+
+    def post(self):
+        try:
+            form_data = request.get_json()
+            new_subscription = Subscription(
+                description = form_data['description'],
+                subtotal_price = form_data['subtotal_price']
+            )
+            db.session.add(new_subscription)
+            db.session.commit()
+            new_subscription_dict = new_subscription.to_dict()
+            response = make_response(
+                new_subscription_dict,
+                201
+            )
+        except:
+            response = make_response(
+                {'error': 'Could not create subscription'},
+                400
+            )
+        return response
+
+api.add_resource(Subscriptions, '/subscriptions')
+
+# '/subscription/<int:id>' route
+class SubscriptionByID(Resource):
+    def get(self, id):
+        subscription = Subscription.query.filter_by(id=id).first()
+        sub_dict = subscription.to_dictt()
+        response = make_response(
+            sub_dict,
+            200
+        )
+        return response
+
+    def patch(self, id):
+        try:
+            form_data = request.get_json()
+            subscription = Subscription.query.filter_by(id=id).first()
+            for attr in form_data:
+                setattr(subscription, attr, form_data[attr])
+            db.session.commit()
+            subscription_dict = subscription.to_dict()
+            response = make_response(
+                subscription_dict,
+                200
+            )
+        except:
+            response = make_response(
+                {'error': 'Could not update subscription'},
+                400
+            )
+        return response
+    
+    # def delete(self, id):
+api.add_resource(SubscriptionByID, '/subscriptions/<int:id>')
+
+# '/boxes' route
+class Boxes(Resource):
+    def get(self):
+        boxes = [box.to_dict() for box in Box.query.all()]
+        response = make_response(
+            boxes,
+            200
+        )
+        return response
+    
+    def post(self):
+        try:
+            form_data = request.get_json()
+            new_box = Box(
+                name = form_data['name'],
+                included_items = form_data['included_items'],
+                subscription_id = form_data['subscription_id']
+            )
+            db.session.add(new_box)
+            db.session.commit()
+            new_box_dict = new_box.to_dict()
+            response = make_response(
+                new_box_dict,
+                201
+            )
+        except:
+            response = make_response(
+                {'error': 'Could not create box'},
+                400
+            )
+        return response
+# '/boxes/<int:id>' route
+class BoxByID(Resource):
+    def get(self, id):
+        box = Box.query.filter_by(id=id).first()
+        box_dict = box.to_dict()
+        response = make_response(
+            box_dict,
+            200
+        )
+        return response
+
+    def patch(self, id):
+        try:
+            form_data = request.get_json()
+            box = Box.query.filter_by(id=id).first()
+            for attr in form_data:
+                setattr(box, attr, form_data[attr])
+            db.session.commit()
+            box_dict = box.to_dict()
+            response = make_response(
+                box_dict,
+                200
+            )
+        except:
+            response = make_response(
+                {'error': 'Could not update box'},
+                400
+            )
+        return response
+
+
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -1,23 +1,19 @@
 from sqlalchemy_serializer import SerializerMixin
-# from sqlalchemy.ext.associationproxy import association_proxy
-from flask import validates
-
+from sqlalchemy.orm import validates
 from config import db
+# from sqlalchemy.ext.associationproxy import association_proxy
 
 # Models go here!
-
 # User Model
-
-
 class User(db.Model, SerializerMixin):
+    # Table
     __tablename__ = "user"
-
+    # Columns
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True)
     password = db.Column(db.String)
     email = db.Column(db.String)
     total_active_orders = db.Column(db.Integer)
-
     # Relationships
     orders = db.relationship('Order', back_populates='user')
     # Serializers
@@ -30,21 +26,19 @@ class User(db.Model, SerializerMixin):
         else:
             return email
 
-
+# Order Model
 class Order(db.Model, SerializerMixin):
+    # Table
     __tablename__ = "order"
-
+    # Columns
     id = db.Column(db.Integer, primary_key=True)
-
-    # Foreign Key to User Model
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    subscription_id = db.Column(db.Integer, db.ForeignKey("subscription.id"))
-
     status = db.Column(db.String)
     frequency = db.Column(db.String)
     quantity = db.Column(db.Integer)
     total_monthly_price = db.Column(db.Float)
-
+    # Foreign Keys
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    subscription_id = db.Column(db.Integer, db.ForeignKey("subscription.id"))
     # Relationships
     user = db.relationship('User', back_populates='orders')
     subscription = db.relationship('Subscription', back_populates='orders')
@@ -58,14 +52,14 @@ class Order(db.Model, SerializerMixin):
         else:
             return value
 
-
+# Subscription model
 class Subscription(db.Model, SerializerMixin):
+    # Table
     __tablename__ = "subscription"
-
+    # Columns
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String)
     subtotal_price = db.Column(db.Float)
-
     # Relationships
     orders = db.relationship('Order', back_populates='subscription')
     box = db.relationship('Box', back_populates='subscription')
@@ -79,17 +73,16 @@ class Subscription(db.Model, SerializerMixin):
         else:
             return value
 
-
+# Box model
 class Box(db.Model, SerializerMixin):
+    # Table
     __tablename__ = "box"
-
+    # Columns
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     included_items = db.Column(db.String)
-
-    # Foreign Key to Subscription Model
+    # Foreign Key
     subscription_id = db.Column(db.Integer, db.ForeignKey("subscription.id"))
-
     # Relationships
     subscription = db.relationship('Subscription', back_populates='box')
     # Serializers
